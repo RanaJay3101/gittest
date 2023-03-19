8ea854c86f53f3278d85b9f88a417ed4ef7de0e9
Rana Jayyyy <20bce241@nirmauni.ac.in>
2023-03-19 23:11:47 +0530

    ranajay

---
+++ rjtesting1.cpp
@@ -0,0 +1,24 @@
+/* Created By Rana Jay on 27-01-2023 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define hurryUp ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define ll long long int
+#define li cout<<"\n"
+#define mod 1000000007
+
+
+
+int main()
+{
+    hurryUp;
+    ll T;
+    cin>>T;
+    while(T--){
+        
+    }
+    return 0;
+}
+
